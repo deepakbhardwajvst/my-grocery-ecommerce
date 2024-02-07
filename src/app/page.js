3da9d98d9b7a8e413a5c29603a27bce7b5019d1b
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-02-07 17:45:31 +0530

    updated page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,13 @@ import Header from "@/Components/Header/Header";
 
 export default function Home() {
   return (
-<<<<<<< HEAD
+
     <div>
       <Header />
 
-=======
     <div className='main'>
->>>>>>> 1e58ea0938468b81ca2a0c32b77a8edebb72cb63
-      <HomePage />
+
+      <HomePage /></div>
       <Footer />
     </div>
   );

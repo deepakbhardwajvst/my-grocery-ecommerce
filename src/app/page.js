a3c36fd33313ef56f98cf10e6e8f2690dade7708
Deepak Mishra <deepakbhardwajvst@gmail.com>
2024-02-08 20:58:52 +0530

    added data of db.json using useEffect, and managing the data state by useState

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,31 @@
-
+"use client"
+import { useEffect, useState } from "react";
 import HomePage from "../pages/Homepage/HomePage";
-
+import axios from "axios";
 
 export default function Home() {
+  const [productData, setProductData] = useState()
+  useEffect(() => {
+    getData('http://localhost:3000/productData')
+  })
+  const getData = async (url) => {
+    try {
+      await axios.get(url).then((res) => {
+        console.log(res.data)
+      })
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
   return (
 
     <div>
-      
 
-    <div className='main'>
 
-      <HomePage /></div>
-     
+      <div className='main'>
+
+        <HomePage /></div>
+
     </div>
   );
 }
